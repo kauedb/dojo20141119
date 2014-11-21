d95e7e9221bb8ebcbe76ec8d74063234a54b7a52
kauedb <kauedb@gmail.com>
2014-11-21 14:47:03 -0200

    formatting AlgarismoRomanoTest

--- test/dojo/AlgarismoRomanoTeste.groovy
+++ test/dojo/AlgarismoRomanoTeste.groovy
@@ -9,46 +9,54 @@ import org.testng.Assert
 class AlgarismoRomanoTeste {
 
     @Test
-    public void dadoIRetornar1(){
+    public void dadoIRetornar1() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('I'), 1);
     }
+
     @Test
-    public void dadoVRetornar5(){
+    public void dadoVRetornar5() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('V'), 5);
     }
+
     @Test
-    public void dadoXRetornar10(){
+    public void dadoXRetornar10() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('X'), 10);
     }
+
     @Test
-    public void dadoLRetornar50(){
+    public void dadoLRetornar50() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('L'), 50);
     }
+
     @Test
-    public void dadoCRetornar100(){
+    public void dadoCRetornar100() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('C'), 100);
     }
+
     @Test
-    public void dadoDRetornar500(){
+    public void dadoDRetornar500() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('D'), 500);
     }
+
     @Test
-    public void dadoMRetornar1000(){
+    public void dadoMRetornar1000() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('M'), 1000);
     }
+
     @Test
-    public void dadoIIIRetornar3(){
+    public void dadoIIIRetornar3() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('III'), 3);
     }
-    public void dadoXXXRetornar30(){
+
+    public void dadoXXXRetornar30() {
         AlgarismoRomano conversor = new AlgarismoRomanoImpl()
         Assert.assertEquals(conversor.paraArabico('XXX'), 30);
     }
